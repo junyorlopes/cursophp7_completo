a527f9a77bd5441863fe1182afaf78c13664298c
Junyor Lopes <carloslopesjunyor@gmail.com>
2017-11-17 14:36:41 -0200

    add biblioteca GD

---
+++ GD/exemplo-02.php
@@ -0,0 +1,14 @@
+<?php
+$image = imagecreatefromjpeg("certificado.jpg");
+$titleColor = imagecolorallocate($image, 0, 0, 0);
+$gray = imagecolorallocate($image, 100, 100, 100);
+
+imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
+imagestring($image, 5, 440, 350, "Carlos Pereira Lopes", $titleColor);
+imagestring($image, 3, 440, 370, "Concluido em " . date("d/m/Y"), $titleColor);
+
+header("Content-Type: image/png");
+imagejpeg($image, "certificado-".date("d-m-Y").".jpg", 50);
+imagedestroy($image);
+
+?>
